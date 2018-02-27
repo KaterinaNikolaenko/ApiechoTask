0aa2087b10b284437fe632dc4135b058a35a7c03
Katerina <katya-nik@list.ru>
2018-02-27 11:12:26 +0200

    [+] added icons for app

---
+++ ApiechoTask/Helpers/Extensions/UIColor+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Extension.swift
+//  ApiechoTask
+//
+//  Created by Katerina on 27.02.18.
+//  Copyright © 2018 Katerina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    struct CustomColors {
+        static let darkRed = UIColor(red:168.0/255.0, green:30.0/255.0, blue:40.0/255.0, alpha:1.0)
+    }
+}
---
+++ ApiechoTask/Helpers/Extensions/UIView+Constraints.swift
@@ -0,0 +1,63 @@
+//
+//  UIView+Constraints.swift
+//  ApiechoTask
+//
+//  Created by Katerina on 27.02.18.
+//  Copyright © 2018 Katerina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK - UIView + Constraints
+
+extension UIView {
+    
+    @discardableResult func topAnchor(equalTo anchor: NSLayoutYAxisAnchor, constant: CGFloat = 0) -> Self {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        return self
+    }
+    
+    @discardableResult func bottomAnchor(equalTo anchor: NSLayoutYAxisAnchor, constant: CGFloat = 0) -> Self {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        bottomAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        return self
+    }
+    
+    @discardableResult func leadingAnchor(equalTo anchor: NSLayoutXAxisAnchor, constant: CGFloat = 0) -> Self {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        leadingAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        return self
+    }
+    
+    @discardableResult func trailingAnchor(equalTo anchor: NSLayoutXAxisAnchor, constant: CGFloat = 0) -> Self {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        trailingAnchor.constraint(equalTo: anchor, constant: constant).isActive = true
+        return self
+    }
+    
+    @discardableResult func heightAnchor(equalTo height: CGFloat) -> Self {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        return self
+    }
+    
+    @discardableResult func widthAnchor(equalTo height: CGFloat) -> Self {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: height).isActive = true
+        return self
+    }
+    
+    @discardableResult func centerYAnchor(equalTo anchor: NSLayoutYAxisAnchor) -> Self {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: anchor).isActive = true
+        return self
+    }
+    
+    @discardableResult func centerXAnchor(equalTo anchor: NSLayoutXAxisAnchor) -> Self {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: anchor).isActive = true
+        return self
+    }
+}
