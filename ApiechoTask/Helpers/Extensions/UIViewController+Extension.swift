3e8f0415c41a628e563eaa13eba48929d90a4380
Katerina <katya-nik@list.ru>
2018-02-28 09:55:00 +0200

    [u] changed alert func

--- ApiechoTask/Helpers/Extensions/UIViewController+Extension.swift
+++ ApiechoTask/Helpers/Extensions/UIViewController+Extension.swift
@@ -13,9 +13,8 @@ extension UIViewController {
     
     func createAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
-            self.dismiss(animated: true, completion: nil)
-        }))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
+        
     }
 }
